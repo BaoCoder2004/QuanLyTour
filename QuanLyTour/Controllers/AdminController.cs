997897a8e66e65e33ec780ddd75f28dd6a625944
vuthang444 <thangvv.a5k47gtb@gmail.com>
2025-03-20 11:56:29 +0700

    add fix

--- QuanLyTour/Controllers/AdminController.cs
+++ QuanLyTour/Controllers/AdminController.cs
@@ -612,7 +612,7 @@ namespace QuanLyTour.Controllers
 				string rooms = "SELECT p.MaKhachSan, p.TenKhachSan, p.MaLoaiKhachSan, p.TrangThai,p.SoNgay, p.DiaDiem, p.GiaKhachSan, p.HinhAnh1, p.HinhAnh2, p.HinhAnh3, l.TenLoaiKhachSan, p.MoTa, p.LichTrinh " +
 				   "FROM KhachSan p " +
 				   "INNER JOIN LoaiKhachSan l ON p.MaLoaiKhachSan = l.MaLoaiKhachSan " +
-				   "WHERE p.MaKhachSan =@maKhachSan And p.MaLoaiKhachSan = @maLoai";
+				   "WHERE p.MaKhachSan =1 And p.MaLoaiKhachSan = @maLoai";
 				using (var command = new SqlCommand(rooms, connection))
 				{
 					command.Parameters.AddWithValue("@maKhachSan", maKhachSan);
