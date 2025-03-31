c4abe5393a1b4cf4f20893c24247679b74d6aa07
Baocoder18 <kbao230104@gmail.com>
2025-04-01 00:07:16 +0700

    add some change

---
+++ QuanLyTour/Controllers/DanhGiaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyTour.Controllers
+{
+    public class DanhGiaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
