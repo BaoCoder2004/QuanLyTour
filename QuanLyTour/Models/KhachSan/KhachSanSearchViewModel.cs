843b1a34d7fd008458cf65182a323c56feb37b49
Baocoder18 <kbao230104@gmail.com>
2025-04-01 13:41:06 +0700

    new

--- QuanLyTour/Models/KhachSan/KhachSanSearchViewModel.cs
+++ QuanLyTour/Models/KhachSan/KhachSanSearchViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace QuanLyTour.Models.KhachSan
+﻿using X.PagedList;
+using QuanLyTour.Models.KhachSan;
+
+namespace QuanLyTour.Models.KhachSan
 {
     public class KhachSanSearchViewModel
     {
+        public IPagedList<KhachSanViewModel> KhachSans { get; set; }
+        public string Keyword { get; set; }
     }
 }
