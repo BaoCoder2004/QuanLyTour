52f6eec2bb2d05d66d4a0cb8a3c8914dee09ea37
Baocoder18 <kbao230104@gmail.com>
2025-04-01 21:40:50 +0700

    add model VeTau

---
+++ QuanLyTour/Models/VeTau/ThongTinChuyenTau.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class ThongTinChuyenTau
+{
+    public int ChuyenTauID { get; set; }
+
+    public string TenChuyenTau { get; set; }
+
+    public string GaDi { get; set; }
+
+    public string GaDen { get; set; }
+
+    public DateTime NgayKhoiHanh { get; set; }
+
+    public DateTime NgayDen { get; set; }
+
+    public int SoLuongVe { get; set; }
+
+    public decimal GiaVe { get; set; }
+
+    // Liên kết danh sách vé đã đặt
+    public virtual ICollection<ThongTinDatVeTau> DanhSachVe { get; set; }
+}
